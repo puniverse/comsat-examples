e5da0ebfd094462dfd3e7fe297414278d82ab846
eitan <eitan@paralleluniverse.co>
2014-05-07 13:55:08 +0300

    make jdbi interface suspendable by annotation

--- comsat-examples-dropwizard/src/main/java/com/example/helloworld/MyDAO.java
+++ comsat-examples-dropwizard/src/main/java/com/example/helloworld/MyDAO.java
@@ -1,17 +1,18 @@
 package com.example.helloworld;
 
-import co.paralleluniverse.fibers.SuspendExecution;
+import co.paralleluniverse.fibers.Suspendable;
 import org.skife.jdbi.v2.sqlobject.Bind;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 
+@Suspendable
 public interface MyDAO {
     @SqlUpdate("create table something (id int primary key, name varchar(100))")
-    void createSomethingTable() throws SuspendExecution;
+    void createSomethingTable();
 
     @SqlUpdate("insert into something (id, name) values (:id, :name)")
-    void insert(@Bind("id") int id, @Bind("name") String name) throws SuspendExecution;
+    void insert(@Bind("id") int id, @Bind("name") String name);
 
     @SqlQuery("select name from something where id = :id")
-    String findNameById(@Bind("id") int id) throws SuspendExecution;
+    String findNameById(@Bind("id") int id);
 }
