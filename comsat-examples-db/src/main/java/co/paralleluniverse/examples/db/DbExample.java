20e383d875ae8e3f349324c45f00c6efca8cf1b6
eitan <eitan@paralleluniverse.co>
2014-06-01 19:23:30 +0300

    enhance db example to jdbi

--- comsat-examples-db/src/main/java/co/paralleluniverse/examples/db/DbExample.java
+++ comsat-examples-db/src/main/java/co/paralleluniverse/examples/db/DbExample.java
@@ -2,12 +2,22 @@ package co.paralleluniverse.examples.db;
 
 import co.paralleluniverse.fibers.Fiber;
 import co.paralleluniverse.fibers.SuspendExecution;
+import co.paralleluniverse.fibers.Suspendable;
 import co.paralleluniverse.fibers.jdbc.FiberDataSource;
+import co.paralleluniverse.fibers.jdbi.FiberDBI;
 import co.paralleluniverse.strands.SuspendableRunnable;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import java.sql.SQLException;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 import javax.sql.DataSource;
 import org.h2.jdbcx.JdbcDataSource;
+import org.skife.jdbi.v2.Handle;
+import org.skife.jdbi.v2.exceptions.UnableToExecuteStatementException;
+import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import org.skife.jdbi.v2.util.StringMapper;
 
 public class DbExample {
     public static void main(String[] args) throws SQLException, ExecutionException, InterruptedException {
@@ -23,5 +33,41 @@ public class DbExample {
                 System.out.println(res);
             }
         }).start().join();
+
+        final FiberDBI jdbi = new FiberDBI(fiberDataSource, Executors.newFixedThreadPool(10,new ThreadFactoryBuilder().setDaemon(true).build()));
+        final MyDAO dao = jdbi.onDemand(MyDAO.class);
+        new Fiber<Void>(new SuspendableRunnable() {
+            @Override
+            public void run() throws SuspendExecution, InterruptedException {
+                try (Handle h = jdbi.open()) {
+                    try {
+                        h.execute("create table something (id int primary key, name varchar(100))");
+                    } catch (UnableToExecuteStatementException t) {
+                        h.execute("truncate table something"); // in case the table exists
+                    }
+                    for (int i = 0; i < 100; i++)
+                        h.execute("insert into something (id, name) values (?, ?)", i, "stranger " + i);
+                    System.out.println("name37 is: " + h.createQuery("select name from something where id = :id")
+                            .bind("id", 37)
+                            .map(StringMapper.FIRST)
+                            .first());
+                    System.out.println("dao name37: "+dao.findNameById(37));
+                    h.execute("drop table something");
+                }
+            }
+        }).start().join();
+
+    }
+
+    @Suspendable
+    public interface MyDAO {
+        @SqlUpdate("create table something (id int primary key, name varchar(100))")
+        void createSomethingTable();
+
+        @SqlUpdate("insert into something (id, name) values (:id, :name)")
+        void insert(@Bind("id") int id, @Bind("name") String name);
+
+        @SqlQuery("select name from something where id = :id")
+        String findNameById(@Bind("id") int id);
     }
 }
