c128a82efa6510a253febde2298e97db8abafd2f
circlespainter <fabio@paralleluniverse.co>
2015-10-22 15:02:38 +0300

    -

--- test-servlet/src/main/java/co/paralleluniverse/examples/test/TestServlet.java
+++ test-servlet/src/main/java/co/paralleluniverse/examples/test/TestServlet.java
@@ -17,7 +17,7 @@ public class TestServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try (PrintWriter out = resp.getWriter()) {
-            out.println(new Date() + ": test servlet resonse");
+            out.println(new Date() + ": test servlet response");
         }
     }
 }
